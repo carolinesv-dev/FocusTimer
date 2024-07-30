07c720e2a3fbb973c8fa0002a81627946301842e
Carol Silveira <carolinesv.dev@gmail.com>
2024-07-30 14:48:23 -0300

    update

--- src/FocusTimer/sounds7.js
+++ src/FocusTimer/sounds7.js
@@ -2,3 +2,10 @@ export const treePressAudio = new Audio('./assets/tree.wav')
 export const cloudPressAudio = new Audio('./assets/cloud.wav')
 export const storePressAudio = new Audio('./assets/store.wav')
 export const firePressAudio = new Audio('./assets/fire.wav')
+
+treePressAudio.loop = true
+cloudPressAudio.loop = true
+storePressAudio.loop = true
+firePressAudio.loop = true
+
+
